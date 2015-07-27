7c6b639f3b31d515b81b62a79fd36fb5b112e025
Rafael Minguet <rminguet@gmail.com>
2015-07-27 21:44:14 +0200

    Script to figure out how many cells there are around a cell

---
+++ Assets/adjacentCells.js
@@ -0,0 +1,26 @@
+﻿#pragma strict
+
+var radius   : float = 0.0;
+var distance : float = 0.0;
+
+function OnMouseOver()
+{
+    var raycastHits : RaycastHit[] = getAdjacents();
+    for(var go : RaycastHit in raycastHits) {
+        go.transform.gameObject.GetComponent.<Renderer>().material.color = Color.red;
+    }
+    this.gameObject.GetComponent.<Renderer>().material.color = Color.white;
+}
+
+function OnMouseExit()
+{
+    var raycastHits : RaycastHit[] = getAdjacents();
+    for(var go : RaycastHit in raycastHits) {
+        go.transform.gameObject.GetComponent.<Renderer>().material.color = Color.white;
+    }
+}
+
+function getAdjacents()
+{
+    return Physics.SphereCastAll(this.transform.position, radius, this.transform.position + Vector3.up, distance);
+}
